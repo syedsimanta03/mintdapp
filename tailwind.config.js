195b5665e58ddd367c417cc9019f3080ac212cff
sam <syed.simanta06@gmail.com>
2022-04-09 17:32:28 +0600

    'added'

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+ content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+    fontFamily: {
+        coiny: ['Coiny', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'brand-purple': 'var(--clr-purple)',
+        'brand-pink': 'var(--clr-pink)',
+        'brand-yellow': 'var(--clr-yellow)',
+        'brand-blue': 'var(--clr-blue)',
+        'brand-green': 'var(--clr-green)',
+        'brand-light': 'var(--clr-light)',
+        'brand-background': 'var(--clr-background)'
+      },
+      animation: {
+        'pulse-slow': 'pulse 10s linear infinite'
+      }
+    },
+  },
+  plugins: [],
+}
